bc479fbdaf5956bcaafc409af378eebe7afd6e19
yang.song <yang.song@17zuoye.com>
2023-08-21 18:14:52 +0800

    feat: 修改target为es2015

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   build: {
     reportCompressedSize: false,
+    target: 'es2015',
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
       name: 'DevtoolsForbidden',
